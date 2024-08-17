b62f7a65cbcf75ca209b3d9bccc21250822d93cf
mempou <glish605@gmail.com>
2024-08-17 10:58:28 +0200

    build folder send online

---
+++ build/precache-manifest.5bef97f00aeb16bdd2596611f88190ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e66dc9fc3ce76ff89c29aafc11f96b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98ed1b7c43d2da2b8584",
+    "url": "/static/css/2.caddc993.chunk.css"
+  },
+  {
+    "revision": "98ed1b7c43d2da2b8584",
+    "url": "/static/js/2.f570269c.chunk.js"
+  },
+  {
+    "revision": "f3364ab5a3ba2fc6cbd0471e6980f1a0",
+    "url": "/static/js/2.f570269c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f6ce0b0b8c5f16a402",
+    "url": "/static/js/main.3e418335.chunk.js"
+  },
+  {
+    "revision": "cf5d58dd4780ebbd3d65",
+    "url": "/static/js/runtime-main.4dc1eb18.js"
+  }
+]);
